83777e3f964794224ab3a9892815dd9713a33498
Jesper van Daatselaar <jesperdaatselaar@gmail.com>
2021-01-29 14:20:38 +0100

    Setup server

---
+++ main.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use("/public", express.static("/src/public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/src/views"));
+
+const db = require("./src/app/login/model");
+const User = db.user;
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Resync Db");
+  initial();
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Routes
+require("./src/app/login/routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+function initial() {
+  User.create({
+    firstName: "Roelie",
+    lastName: "Janssen",
+    email: "info@voetreflexraccoon.nl",
+    password: "ditisnietveilig",
+  });
+  User.create({
+    firstName: "Jesper",
+    lastName: "van Daatselaar",
+    email: "1002003@hr.nl",
+    password: "ditisnietveilig",
+  });
+}
